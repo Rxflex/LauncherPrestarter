bbc25007c0e6f165f4ef52eb70bf5b2a9fe9b65a
Andy <39628283+Rxflex@users.noreply.github.com>
2025-06-20 02:43:21 +0200

    Update PrestarterForm.Designer.cs

--- Prestarter/PrestarterForm.Designer.cs
+++ Prestarter/PrestarterForm.Designer.cs
@@ -55,7 +55,7 @@
             this.logoLabel.Name = "logoLabel";
             this.logoLabel.Size = new System.Drawing.Size(236, 37);
             this.logoLabel.TabIndex = 1;
-            this.logoLabel.Text = "Lump";
+            this.logoLabel.Text = "NekoLand";
             // 
             // statusLabel
             // 
@@ -105,7 +105,7 @@
             this.Margin = new System.Windows.Forms.Padding(2);
             this.Name = "PrestarterForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Lump Prestarter";
+            this.Text = "NekoLand Prestarter";
             this.Load += new System.EventHandler(this.PrestarterForm_Load);
             this.MouseDown += new System.Windows.Forms.MouseEventHandler(this.PreStartedForm_MouseDown);
             this.MouseMove += new System.Windows.Forms.MouseEventHandler(this.PreStartedForm_MouseMove);
